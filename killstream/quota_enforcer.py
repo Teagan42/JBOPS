cd247f280315eb271ee5abbfc6fdc4f5797523c8
Teagan glenn <that@teagantotally.rocks>
2025-03-25 01:00:47 -0600

    Split out query params

--- killstream/quota_enforcer.py
+++ killstream/quota_enforcer.py
@@ -98,8 +98,16 @@ async def is_active_patron(
     headers = {"Authorization": f"Bearer {access_token}"}
 
     # Request all memberships (you may need to paginate if you have too many donors, you popular beast)
-    url = "https://www.patreon.com/api/oauth2/v2/campaigns/2900318/members?include=user&fields[user]=email&fields[member]=patron_status"
-    r = await session.get(url, headers=headers)
+    url = "https://www.patreon.com/api/oauth2/v2/campaigns/2900318/members"
+    r = await session.get(
+        url,
+        headers=headers,
+        params={
+            "include": "user",
+            "fields[user]": "email",
+            "fields[member]": "patron_status",
+        },
+    )
     r.raise_for_status()
     data = r.json()
 
